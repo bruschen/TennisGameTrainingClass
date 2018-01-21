43632d726ad99c0061be8ce45d633fd6731085f1
bruschen <imchengr@gmail.com>
2018-01-21 10:16:44 +0800

    Add project files.

---
+++ TennisGameTrainingClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisGameTrainingClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisGameTrainingClass")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49bd283d-336d-4fa0-89b6-8784f1b20f0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TennisGameTrainingClass/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameTrainingClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Love_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            var expected = tennisGame.GetScore();
+            var actual = "Love_All";
+
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Thirty_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            tennisGame.SetHomeScore(1);
+            var expected = tennisGame.GetScore();
+            var actual = "Thirty_All";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Forty_All()
+        {
+            TennisGame tennisGame = new TennisGame();
+            tennisGame.SetHomeScore(2);
+            var expected = tennisGame.GetScore();
+            var actual = "Forty_All";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Love_Fifteen()
+        {
+            TennisGame tennisGame = new TennisGame();
+            tennisGame.SetHomeScore(0);
+            tennisGame.SetAwayScore(1);
+            var expected = tennisGame.GetScore();
+            var actual = "Forty_All";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class TennisGame
+    {
+        private int HomeCurrentScore;
+
+        public object GetScore()
+        {
+            if (this.HomeCurrentScore==0)
+            {
+                return "Love_All";
+            }
+            else if (this.HomeCurrentScore==1)
+            {
+                return "Thirty_All";
+            }
+            else if (this.HomeCurrentScore==2)
+            {
+                return "Forty_All";
+            }
+
+            return "Love_All";
+        }
+
+        public void SetHomeScore(int score)
+        {
+            this.HomeCurrentScore = score;
+        }
+
+        public void SetAwayScore(int score)
+        {
+            this.SetAwayScore(score);
+        }
+    }
+}
